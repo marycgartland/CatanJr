a0e51038d4e3bdcdb2e8df4c82b7a65cf41cc5cb
marycgartland <mary.gartland@ucdconnect.ie>
2021-11-10 19:41:15 +0000

    Updating the turns the player can take, most specifically trading options; adding in error checks etc.

--- CatanJr/src/gameplay/PlayerTurn.java
+++ CatanJr/src/gameplay/PlayerTurn.java
@@ -12,6 +12,13 @@ public class PlayerTurn {
 	private int marketPlaceUse;	// Integer variable to keep track of how many times the marketplace has been used this turn 
 	private boolean turn;		// turn = true if they are taking their turn, turns to false if they are finished their turn 
 	private int diceValue;		// Value rolled on the dice 
+	private boolean tradeOutValid;
+	private boolean tradeInValid;
+	private String tradeInResource;
+	private String tradeOutResource;
+	private String tradeOutItem;
+	private String tradeInItem;
+
 	// private Player player;
 	
 	//-----------------------------------------------------------
@@ -47,14 +54,17 @@ public class PlayerTurn {
 			//----- If the player wants to buy ----------------------------
 			if(option.equals("B")) {
 				System.out.print("You have chosen to buy.\n");
+				// Call the buy method
 			} 	
 			//----- If the player wants to build --------------------------
 			else if(option.equals("Bd")) {
 				System.out.print("You have chosen to build.\n");
+				// Call the build method 
 			}
 			//----- If the player wants to trade --------------------------
 			else if(option.equals("T")) {
 				System.out.print("You have chosen to Trade.\n");
+				Trade();	// Call the trade method 
 			}
 			//----- If the player wants to end their turn -----------------
 			else if(option.equals("E")) {
@@ -65,19 +75,132 @@ public class PlayerTurn {
 			else{
 				System.out.println("You did not select one of the available options. Please try again. ");
 			}
-		}	
+		}
 	}
 	
-	// Buy method: This method lets you buy cocotiles
-	// Build method: This method lets you build lairs/ships on the board
-	// Trade method
-	// They can also choose to end turn 
-	// During turn, can be checking to see if they have won? Don't know where this happens 
+	//-----------------------------------------------------------
+	//---------- Trade Method -----------------------------------
+	//-----------------------------------------------------------
+	public void Trade() {
+		//----- Give the user options on what trade they wish to make -----
+		if(marketPlaceUse == 0) {	// You can only trade with the marketplace once per turn
+			System.out.print("Would you like to trade with the marketplace [M], stockpile [S], or another player[O]? ");
+		} else {
+			System.out.print("Would you like to trade with the stockpile [S] or another player [O]? ");
+		}
+		Scanner scan = new Scanner(System.in);
+		String option = scan.next();
+		
+		//----- Option 1: Trade with Marketplace --------------------------
+			// Trade one tile from a players pocket (of choice) with one tile from marketplace (of choice)
+			// Can only trade with Marketplace once per turn 
+		if(option.equals("M") && marketPlaceUse == 0) {
+			System.out.print("You have chosen to trade with the marketplace.\n");
+			marketPlaceUse = 1;
+		}
+		
+		//----- Option 2: Trade with Stockpile ----------------------------
+			// can trade 2 of the same pocket tiles of choice with 1 tile from the stockpile
+			// No limit on times, only limited by resources in pocket/stockpile
+		else if(option.equals("S")) {
+			System.out.print("You have chosen to trade with the stockpile.\n");
+			// Show them which items they have 2 of that they can trade
+			tradeOut();	// Let user select what Item they wish to obtain from the stockpile
+			tradeIn();	// Let user select what item they wish to push into the stockpile in return 
+		}
+		
+		//----- Option 3: Trade with another player -----------------------
+		else if (option.equals("O")) {
+			System.out.print("You have chosen to trade with the another player.\n");
+		}
+
+		//----- Error check if the user selects an invalid option ---------
+		else{
+			System.out.print("You did not select one of the available options. Please try again. ");
+		}
+
+	}
 	
+	
+	//-----------------------------------------------------------
+	//---------- Method: TradeIn --------------------------------
+	//-----------------------------------------------------------
+	
+	public String tradeIn() {
+		tradeInValid = false;
+		while(tradeInValid == false) {
+			System.out.print("What item would to trade in (Wood [W], Molasses [M], Gold [G], or Cutlass [C])? ");
+			Scanner scanTI = new Scanner(System.in);
+			tradeInItem = scanTI.next();
+			tradeInValid = validResourceCheck(tradeInItem);
+		}
+		
+		// Assign the word value to the letter entered
+		if(tradeInItem.equals("W")) {
+			tradeInResource = "Wood";
+		} else if (tradeInItem.equals("M")) {
+			tradeInResource = "Molasses";
+		} else if(tradeInItem.equals("G")) {
+			tradeInResource = "Gold";
+		} else {
+			tradeInResource = "Cutlass";
+		}
+		
+		System.out.print("You have chosen to trade in " + tradeInResource + "\n");
+		
+		// Return the selected resource to the user
+		return tradeInResource;
+	}
+	
+	//-----------------------------------------------------------
+	//---------- Method: TradeOut --------------------------------
+	//-----------------------------------------------------------
+	public String tradeOut() {
+		tradeOutValid = false;
+		while(tradeOutValid == false) {
+			System.out.print("What item would you like to obtain (Wood [W], Molasses [M], Gold [G], or Cutlass [C])? ");
+			Scanner scanTO = new Scanner(System.in);
+			tradeOutItem = scanTO.next();
+			tradeOutValid = validResourceCheck(tradeOutItem);
+		}
+		
+		// Assign the word value to the letter entered
+		if(tradeOutItem.equals("W")) {
+			tradeOutResource = "Wood";
+		} else if (tradeOutItem.equals("M")) {
+			tradeOutResource = "Molasses";
+		} else if(tradeOutItem.equals("G")) {
+			tradeOutResource = "Gold";
+		} else {
+			tradeOutResource = "Cutlass";
+		}
+		
+		System.out.print("You have chosen to obtain " + tradeOutResource + "\n");
+		
+		// Return the selected resource to the user
+		return tradeOutResource;
+	}
+	
+	
+	//-----------------------------------------------------------
+	//---------- Method: validResourceCheck ---------------------
+	//-----------------------------------------------------------
+	public boolean validResourceCheck(String enteredResource){
+		// If entered resource is  avalide resource return true, otherwise return false
+		if (enteredResource.equals("M") || enteredResource.equals("W") || enteredResource.equals("C") || enteredResource.equals("G")) {
+			return true;
+		} else {
+			System.out.print("Choice is invalid. Please enter a valid choice.\n");
+			return false;
+		}
+		
+	}
 	
 }
 	
 	
+	
+	// During turn, can be checking to see if they have won? Don't know where this happens 
 	
 	//----------------------------------------------------------
 	//---------- "Your-turn" method ----------------------------
@@ -89,27 +212,18 @@ public class PlayerTurn {
 	//----------------------------------
 	//---------- Build Method ----------
 	//----------------------------------
-	
-			// Costs:
-				// Pirates Lair = 1 cutlass, 1 molasses, 1 goat & 1 wood
-				// Ship = 1 goat & 1 wood
-			// If they choose a pirates lair or ship, place these on the board 
-				// Should pirates lair and ships be their own classes and objects probably?
-			// Build must be alternating ship and Lair 
+		// This method lets you build lairs/ships on the board
+		// Costs:
+			// Pirates Lair = 1 cutlass, 1 molasses, 1 goat & 1 wood
+			// Ship = 1 goat & 1 wood
+		// If they choose a pirates lair or ship, place these on the board 
+			// Should pirates lair and ships be their own classes and objects probably?
+		// Build must be alternating ship and Lair 
 	
 	//----------------------------------
 	//---------- Buy Method ------------
 	//----------------------------------
-			// Cost: Cocotile = 1 cutlass, 1 molasses & 1 gold 
-			// If the player chooses a cocotile, give them a cocotile, and follow instructions on cocotile
+		// This Method lets you buy cocotiles 
+		// Cost: Cocotile = 1 cutlass, 1 molasses & 1 gold 
+		// If the player chooses a cocotile, give them a cocotile, and follow instructions on cocotile
 	
-	//----------------------------------
-	//---------- Trade Method ----------
-	//----------------------------------
-			// Option 1: Trade with Marketplace
-				// Trade one tile from a players pocket (of choice) with one tile from marketplace (of choice)
-				// Can only trade with Marketplace once per turn 
-			// Option 2: Trade with stockpile 
-				// can trade 2 of the same pocket tiles of choice with 1 tile from the stockpile
-				// No limit on times, only limited by resources in pocket/stockpile
-			// Option 3: Trade with another player
