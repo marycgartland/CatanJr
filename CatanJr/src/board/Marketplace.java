f428f4f3cabc39a5b8583d045b67583d44927c63
emmapender101 <emma.pender@ucdconnect.ie>
2021-11-10 11:30:14 +0000

    setting up marketplace and stockpile(incomplete)

--- CatanJr/src/board/Marketplace.java
+++ CatanJr/src/board/Marketplace.java
@@ -1,11 +1,79 @@
 package board;
 
 public class Marketplace {
-	
+
+	protected Resources[] marketPlace = new Resources[5]; // marketplace will be made up of an array of resources (chose an array cause its a set length, arraylists can change in length)
+
 	// Variables
 	// Constructor
-	// Keep track so there is only 1 swap with the marketplace per turn
-	// If all 5 places are the same resource, then we need to reset the marketplace - need a check and a reset method
+	// If all 5 places are the same resource, then we need to reset the marketplace
+	// - need a check and a reset method
 	// method to let the user trade
+
+	// SetupMethod which will be called by the setup class: this will restock the
+	// market place with one of each resource (1 x: cutlass, goat, wood, gold and
+	// molasses)
+
+	// CheckMethod to check that all resources in the marketplace are not the same,
+	// if it is the Setup Method is called and the all resources are returned to
+	// stockpile
+
+
+	// Swap method that can only be called once in a turn which tracks resources in
+	// marketplace
+	
+	// EP: YourTurn Class should keep track of how many times the marketplace is swapped with per users turn
+
+
+	public Marketplace() {
+	}
+
+	public void SetupMarketplace() {
+		this.marketPlace = [Resources.Wood, Resources.Cutlasses, Resources.Goats, Resources.Gold, Resources.Molasses];
+		// this takes resources from the stockpile so need to update the stockpile
+	}
+
+	// Method to check and see if a specified resource is in the marketplace if the
+	// user wants to trade
+	public boolean CheckForResourceMarketplace(Resources resource) {
+		for (int i = 0; i <= 4; i++) {
+			if (marketPlace[i] == resource) {
+				System.out.println("The Marketplace has this resource");
+				// ask the user if they want to swap with the marketplace, if yes then call
+				// SwapMethod
+				return true;
+			} else {
+				System.out.println("The Marketplace does not have resource");
+				return false;
+			}
+		}
+	}
+
+	// Method to swap a resource for another
+	public void SwapMarketplace(Resources wantedResource, Resources toSwapResource) {
+		for (int i = 0; i <= 4; i++) {
+			if (marketPlace[i] == wantedResource) {
+				marketPlace[i] = toSwapResource; // this updates the marketplace resources, need to update users pocket to give them this variable
+				CheckMarketplace(); // now that user has swapped with the marketplace, need to check and make sure all marketplace resources are not the same type
+
+			}
+		}
+	}
+
+	// checks to see if there are 5 of the same element or not
+	public void CheckMarketplace() {
+		int counter = 0;
+		for (int i = 0; i <= 3; i++) {
+			if (marketPlace[i] == marketPlace[i + 1]) {
+				counter = counter + 1;
+			}
+		}
+		if (counter == 4) { // all array elements are equal
+			// need to return all resources to stockpile
+			SetupMarketplace(); // setup marketplace again with the 5 different elements
+		} else {
+			// all elements are not equal
+		}
+	}
 
 }
--- CatanJr/src/board/Stockpile.java
+++ CatanJr/src/board/Stockpile.java
@@ -1,11 +1,59 @@
 package board;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Stockpile {
-	
+
 	// Variables
 	// Constructor
 	// Method to let user swap 2 resources for 1 in the stockpile
 	// Keep track of the number of each resource
-	// Method to make sure there is at least 1 of each resource left, otherwise all of that resource are returned to the stockpile 
+	// Method to make sure there is at least 1 of each resource left, otherwise all
+	// of that resource are returned to the stockpile
+
+	// EP: Stockpile needs to be called before the users are given their first pieces and before the marketplace is setup
+	// EP: Need method to assign resources to different users, for example when the dice is rolled, the stockpile needs to assign the resources to the players
+
+	protected HashMap<Resources, Integer> stockpile = new HashMap<Resources, Integer>();
+
+	// Constructor
+	public Stockpile() {
+		// add 18 of each resource to stockpile
+		stockpile.put(Resources.Gold, 18);
+		stockpile.put(Resources.Wood, 18);
+		stockpile.put(Resources.Cutlasse, 18);
+		stockpile.put(Resources.Molasses, 18);
+		stockpile.put(Resources.Goats, 18);
+	}
+
+	// if this method is called 2 of the toSwapResource needs to be removed from the
+	// users pocket and 1 of the wantedResource needs to be added to their pocket
+	public void SwapStockpile(Resources wantedResource, Resources toSwapResource) {
+		int numberofwantedresource = stockpile.get(wantedResource);
+
+		if (numberofwantedresource > 0) {
+			stockpile.put(wantedResource, numberofwantedresource - 1); // need to add 1 of this resource to users pocket
+			stockpile.put(toSwapResource, stockpile.get(toSwapResource) + 2); // need to remove 2 of this resource from users pocket
+		}
+		else {
+			CheckStockpile(); // need to call method to check if there is more than 0 of each resource, if not all of that resource must be returned
+		}
+
+	}
+	// method to check if resources need to be returned to stockpile
+	public void CheckStockpile() {
+		if(stockpile.get(Resources.Gold)==0) {
+			// make method to take back all of this resource
+		} else if (stockpile.get(Resources.Molasses)==0) {
+			// make method to take back all of this resource
+		} else if (stockpile.get(Resources.Wood)==0) {
+			// make method to take back all of this resource
+		}else if (stockpile.get(Resources.Cutlasses)==0) {
+			// make method to take back all of this resource
+		}else if (stockpile.get(Resources.Goats)==0) {
+			// make method to take back all of this resource
+		}
+	}
 
 }
