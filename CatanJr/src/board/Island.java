b9d139c0bc62c9b4cacda2f20cc1f370295f9dac
marycgartland <mary.gartland@ucdconnect.ie>
2021-12-04 14:41:36 +0000

    Moving checkArray() method from Board class to Island class

--- CatanJr/src/board/Island.java
+++ CatanJr/src/board/Island.java
@@ -1,5 +1,11 @@
 package board;
 
+import java.util.ArrayList;
+
+import main.Interactor;
+import player.Player;
+import resources.Resources;
+
 // ----------------------------------------------------------------------------------------
 // This is a class to construct the islands on the board. The goal of this class is to set 
 // the lair locations connected to each island, in terms of their position on the matrix.
@@ -11,8 +17,16 @@ public class Island {
 	// --------------------------------
 	// ----------- Variables ----------
 	// --------------------------------
-	private int[] columns;	// Array of column values
-	private int[] rows;		// Array of row values
+	private int[] columns;									// Array of column values
+	private int[] rows;										// Array of row values
+	protected Interactor interactor;						// Interactor to work with user
+	protected char[] playerColors = {'B','R', 'W', 'O'};	// Array of color letters as appears on the board
+	protected char playerColor;								// Players color
+	protected Player player;								// Selected player
+	protected char isGhost; 								// Character to see if the ghost captain is there
+	protected int temp_row;									// Selected row value
+	protected int temp_col;									// Selected column value
+	private String playerName;								// Players name
 	
 	// ----------------------------------
 	// ----------- Constructor ----------
@@ -20,6 +34,7 @@ public class Island {
 	public Island(int[] row_vals, int[] col_vals) {
 		this.columns = col_vals;
 		this.rows = row_vals;
+		interactor = new Interactor();
 	}
 	
 	// ----------------------------------
@@ -32,5 +47,30 @@ public class Island {
 	public int[] getRow() {		// Get row value
 		return rows;
 	}
+	
+	//-----------------------------------------------------------------------------------------------
+	//---------- Method: checkArray() ---------------------------------------------------------------
+	// This method checks what players have a Lair touching an island and assigns resources as needed
+	//-----------------------------------------------------------------------------------------------
+	public void checkArray(ArrayList<Player> players, Resources resource, char isGhost, char[][] design){
+		if(isGhost != 'G') {
+			for (int j = 0; j <= players.size() - 1; j++) {
+				playerColor = playerColors[j];
+				player = players.get(j);
+				for (int i = 0; i <= columns.length - 1; i++) {
+					temp_row = rows[i];
+					temp_col = columns[i];
+					if(design[temp_row][temp_col]==playerColor) {
+						player.addResource(resource, 1);
+						playerName = player.getName();
+						interactor.printMessage("Island check: success", resource, playerName);
+						System.out.println(player.viewPocket());
+						}
+					}
+				}
+			} else {	// If the ghost captain is on an island, no resources are distributed from it
+				interactor.printMessage("Island check: ghost captain");
+			}
+		}
 
 }
