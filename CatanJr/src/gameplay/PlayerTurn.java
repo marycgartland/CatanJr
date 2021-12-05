3b528b18e87dd1ee3912eab03c6502f63745024b
marycgartland <mary.gartland@ucdconnect.ie>
2021-12-05 00:22:56 +0000

    Tidy up PlayerTurn class

--- CatanJr/src/gameplay/PlayerTurn.java
+++ CatanJr/src/gameplay/PlayerTurn.java
@@ -14,16 +14,16 @@ public class PlayerTurn {
 	// ------------------------------------------------------------------------
 	// ---------- Variables ---------------------------------------------------
 	// ------------------------------------------------------------------------
-	private int marketPlaceUse; // Number of times the marketplace has been used this turn
-	private boolean turn; // Is 'true' when taking turn, and 'false' when finished turn
-	private int diceValue; // Value rolled on the dice
-	private Player player; // Player taking the turn
-	private String toBuild; // User input answer
-	private String toBuy; // User input answer
-	protected Marketplace marketplace; // Take in marketplace that has been created
-	protected Stockpile stockpile; // Take in stockpile that has been created
-	protected CocoTiles cocotiles; // Take in cocotiles that have been created
-	protected Board board; // Take in board that has been created
+	private int marketPlaceUse;			// Number of times the marketplace has been used this turn
+	private boolean turn; 				// Is 'true' when taking turn, and 'false' when finished turn
+	private int diceValue; 				// Value rolled on the dice
+	private Player player; 				// Player taking the turn
+	private String toBuild; 			// User input answer
+	private String toBuy; 				// User input answer
+	protected Marketplace marketplace; 	// Take in marketplace that has been created
+	protected Stockpile stockpile; 		// Take in stockpile that has been created
+	protected CocoTiles cocotiles; 		// Take in cocotiles that have been created
+	protected Board board; 				// Take in board that has been created
 
 	// ------------------------------------------------------------------------
 	// ----------- Constructor ------------------------------------------------
@@ -51,10 +51,8 @@ public class PlayerTurn {
 		// ----- Roll the dice ----------------------------------------------
 		diceValue = dice1.rollDice();
 		interactor.printMessage("Player roll", player.PlayerName(), diceValue);
-
 		// ----- Resource distribution from islands based on dice roll ------
 		board.islandResourceDistribution(diceValue, islands, players);
-
 		// ----- Present users with options while it is their turn ------------
 		while (turn == true) {
 			interactor.printMessage("your turn");		// Provide user options and take in their choice
@@ -84,37 +82,31 @@ public class PlayerTurn {
 	// followed.
 	// ------------------------------------------------------------------------
 	public void buy() {
-		// Display cocotile price to user
+		// ----- Display cocotile price to user ---------------------------------
 		interactor.printMessage("cocotile price");
-		
-		// If the player has enough resources they can buy a cocotile
+		// ----- If the player has enough resources they can buy a cocotile -----
 		if (player.getCutlassesCount() >= 1 && player.getGoldCount() >= 1 && player.getMolassesCount() >= 1) {
-			
-			// Give player a chance to confirm they wish to buy a cocotile
+			// ----- Give player a chance to confirm they wish to buy a cocotile -----
 			interactor.printMessage("buy cocotile?");	
 			toBuy = interactor.takeInAnswer();			
 			if (toBuy.equals("Y") || toBuy.equals("y")) {	
 				interactor.printMessage("bought cocotile");	
-				
-				// Remove cost from users pocket
+				// ----- Remove cost from users pocket --------------------------------
 				player.removeResource(Resources.Cutlasses, 1);
 				player.removeResource(Resources.Gold, 1);
 				player.removeResource(Resources.Molasses, 1);
-				
-				// Return resources to stockpile when cocotile is bought
+				// ----- Return resources to stockpile when cocotile is bought --------
 				stockpile.ReturnResource(Resources.Cutlasses, 1);
 				stockpile.ReturnResource(Resources.Gold, 1);
 				stockpile.ReturnResource(Resources.Molasses, 1);
-				
-				// Cocotile bought
+				// ----- Cocotile bought ----------------------------------------------
 				CocoTileTypes cocotile_bought = cocotiles.buyCocoTile(); 	// Indicates type of cocotile
 				player.addCocoTile(); 										// Add cocotile to players cocotile count
 				interactor.printMessage("Cocotile bought", cocotile_bought);// Confirm to user
 				cocotileAction(cocotile_bought); 							// Follow instructions on cocotile
 			}
 		} 
-		
-		// Error message if the user does not have resources needed
+		// ----- Error message if the user does not have resources needed -------
 		else { 
 			interactor.printMessage("cannot buy: cocotile");
 		}
@@ -126,40 +118,30 @@ public class PlayerTurn {
 	// ------------------------------------------------------------------------
 	public void cocotileAction(CocoTileTypes cocotile) {
 		if (cocotile.equals(CocoTileTypes.GoatCutlasses)) {			// The goat and cutlass cocotile
-			
-			// Add resource to users pocket
+			// ----- Add resource to users pocket --------
 			player.addResource(Resources.Goats, 2);
 			player.addResource(Resources.Cutlasses, 2);
-			
-			// Remove resources from stockpile
+			// ----- Remove resources from stockpile -----
 			stockpile.DistributeResource(Resources.Goats, 2);
 			stockpile.DistributeResource(Resources.Cutlasses, 2);
-			
-			// Display pocket to player
+			// ----- Display pocket to player ------------
 			interactor.printPocket(player);
-			
 		} else if (cocotile.equals(CocoTileTypes.WoodMolasses)) {	// The wood and molasses cocotile
-			// Add resources to users pocket
+			// ----- Add resources to users pocket -------
 			player.addResource(Resources.Wood, 2);
 			player.addResource(Resources.Molasses, 2);
-			
-			// Remove resources from stockpile
+			// ----- Remove resources from stockpile -----
 			stockpile.DistributeResource(Resources.Wood, 2);
 			stockpile.DistributeResource(Resources.Molasses, 2);
-			
-			// Display pocket to player
+			// ----- Display pocket to player ------------
 			interactor.printPocket(player);
-			
 		} else if (cocotile.equals(CocoTileTypes.GhostCaptain)) {	// The ghost captain cocotile
-			board.moveGhostCaptain();	// move the GC
-			
-		} else if (cocotile.equals(CocoTileTypes.ShipCastle)) {		// the ship/lair cocotile
-			
-			// Give user ship or lair option
+			board.moveGhostCaptain();	// Move the GC
+		} else if (cocotile.equals(CocoTileTypes.ShipCastle)) {		// The ship/lair cocotile
+			// ----- Give user ship or lair option -------
 			interactor.printMessage("cocotile ship/lair");
 			String build_option = interactor.takeInAnswer();
-			
-			// Build lair or ship
+			// ----- Build lair or ship ------------------
 			if (build_option.charAt(0) == 'L') {
 				board.placeLair(player);	// Build Lair
 			} else if (build_option.charAt(0) == 'S') {
@@ -177,7 +159,7 @@ public class PlayerTurn {
 	// ------------------------------------------------------------------------
 	public void build() {
 		interactor.printMessage("ship/lair price");
-		// If the user has the resources to build either, provide both options
+		// ----- If the user has the resources to build either, provide both options -----
 		if (player.getCutlassesCount() >= 1 && player.getWoodCount() >= 1 && player.getGoatsCount() >= 1 && player.getMolassesCount() >= 1) {
 			interactor.printMessage("build ship/lair");				 // Give user option of building a ship or lair
 			toBuild = interactor.takeInAnswer(); 					 // Scan in user response
@@ -186,16 +168,14 @@ public class PlayerTurn {
 			} else if (toBuild.equals("L") || toBuild.equals("l")) { // Build Lair
 				buildLair();
 			}
-		} 
-		// If the user only has the resources to build a ship
+		} // ----- If the user only has the resources to build a ship --------------------
 		else if (player.getWoodCount() >= 1 && player.getGoatsCount() >= 1) {
 			interactor.printMessage("build ship?");					 // Ask user to confirm they wish to buy a ship
 			toBuild = interactor.takeInAnswer(); 					 // Scan in user input
 			if (toBuild.equals("Y") || toBuild.equals("y")) {		 // Build ship
 				buildShip();					
 			}
-		} 
-		// If they don't have enough resources to build either
+		} // ----- If they don't have enough resources to build either -------------------
 		else { 
 			interactor.printMessage("cannot buy ship/lair");		 // Inform user they do not have enough resources
 		}
@@ -231,7 +211,6 @@ public class PlayerTurn {
 			interactor.printMessage("trade S?");
 		}
 		String option = interactor.takeInAnswer();	// Take in user response
-
 		// ----- Option 1: Trade with Marketplace -----------------------------------
 		// Trade one tile from a players pocket (of choice) with one tile from marketplace (of choice)
 		if ((option.equals("M") || option.equals("m")) && marketPlaceUse == 0) {
@@ -239,7 +218,6 @@ public class PlayerTurn {
 			marketplace.viewMarketplace();	// Let user view marketplace options
 			tradeMarketplace(); 			// Call method to trade with marketplace
 		}
-		
 		// ----- Option 2: Trade with Stockpile -------------------------------------
 		// Trade 2 equivalent pocket tiles (of choice) with 1 stockpile tile (of choice)
 		// No limit on times, only limited by resources in pocket/stockpile. 
@@ -247,18 +225,15 @@ public class PlayerTurn {
 			interactor.printMessage("trade: S");
 			tradeStockpile(); 				// Call method to trade with stockpile
 		}
-
 		// ----- Alt. option 2: Trade with Stockpile when it's the only choice ------
 		else if (marketPlaceUse != 0 && (option.equals("Y") || option.equals("y"))) {
 			interactor.printMessage("trade: S");
 			tradeStockpile(); 				// Call method to trade with stockpile
 		}
-
 		// ----- Option 3: Choose not to trade --------------------------------------
 		else if (option.equals("N") || option.equals("n")) {
 			interactor.printMessage("trade: N");
 		}
-
 		// ----- Error check if the user selects an invalid option ------------------
 		else {
 			interactor.printMessage("turn: null");
@@ -271,29 +246,29 @@ public class PlayerTurn {
 	// This method allows a user to trade with the marketplace
 	// ------------------------------------------------------------------------
 	public void tradeMarketplace() {
-		interactor.printMessage("trade out"); // what item does user want to obtain
+		// ----- Determine what resource user wishes to obtain  - check if their request is in stock -----
+		interactor.printMessage("trade out"); 
 		String trade_out = interactor.takeInAnswer();
 		interactor.printMessage("trade out resource", trade_out);
-		if (validResourceCheck(trade_out)) { // check if trade_out is vaild option
-			boolean in_stock = marketplace.CheckForResourceMarketplace(assignResourcesType(trade_out)); // check if has marketplace has this in stock
+		if (validResourceCheck(trade_out)) { 															
+			boolean in_stock = marketplace.CheckForResourceMarketplace(assignResourcesType(trade_out)); 
 			if (in_stock) {
 				interactor.printMessage("marketplace: in-stock");
-				interactor.printMessage("trade in"); // what item does user want to trade in
+				// ----- Determine what resource user wishes to trade in  - check if they have resource in pocket -----
+				interactor.printMessage("trade in"); 
 				String trade_in = interactor.takeInAnswer();
 				interactor.printMessage("trade in resource", trade_in);
-				if (validResourceCheck(trade_in)) { // check if this trade_in answer is valid
-					if (player.checkPocketResourcesLetter(trade_in) > 0) { // check users pocket to see if they have the
-																			// resources to swap
-						// Can only trade with Marketplace once per turn
-						marketPlaceUse = 1; // successful trade with marketplace
-						marketplace.SwapMarketplace(assignResourcesType(trade_out), assignResourcesType(trade_in),
-								player);
+				if (validResourceCheck(trade_in)) { 
+					if (player.checkPocketResourcesLetter(trade_in) > 0) { 
+						// ----- Make the swap, and increment marketPlaceUse variable to indicate the trade is used (1 per turn) ------
+						marketPlaceUse = 1; 
+						marketplace.SwapMarketplace(assignResourcesType(trade_out), assignResourcesType(trade_in), player);
 						interactor.printPocket(player);
-					} else {
+					} else {	// ----- Cannot trade if pocket doesn't have resource -----
 						interactor.printMessage("cannot trade");
 					}
-				}
-			} else {
+				}	
+			} else {	// ----- Cannot trade if the marketplace is out of stock -----
 				interactor.printMessage("marketplace: out-of-stock");
 			}
 		}
@@ -304,20 +279,18 @@ public class PlayerTurn {
 	// Method to allow a player trade with the stockpile
 	// ------------------------------------------------------------------------
 	public void tradeStockpile() {
+		// ----- Determine what user wishes to trade -----
 		interactor.printMessage("trade out"); 			// Give user option of what to trade out of pocket
 		String trade_out = interactor.takeInAnswer();	// Take in user input 
 		if (validResourceCheck(trade_out)) { 			// Check if trade_out is valid option
 			interactor.printMessage("trade in"); 		// Give user option of what they want to obtain
 			String trade_in = interactor.takeInAnswer();// Take in user input 
-			if (validResourceCheck(trade_in)) {			// Check that the user 
-				if (player.checkPocketResourcesLetter(trade_in) > 1) { // user needs to have more than 2 of the resource
-																		// for a swap with stockpile
-					stockpile.SwapStockpile(assignResourcesType(trade_out), assignResourcesType(trade_in), player); // call
-																													// stockpile
-																													// swap
-																													// method
+			if (validResourceCheck(trade_in)) {			// Check that the user entered a valid resource
+				// ------ User needs 2+ of the resource to swap with stockpile ------
+				if (player.checkPocketResourcesLetter(trade_in) > 1) { 
+					stockpile.SwapStockpile(assignResourcesType(trade_out), assignResourcesType(trade_in), player); 
 					interactor.printPocket(player);
-				} else {
+				} else { // ----- Otherwise, cannot trade -----
 					interactor.printMessage("cannot trade");
 				}
 
