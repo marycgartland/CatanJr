ab6ebb84c9d3cb742c94bc917fe587af55f17f34
marycgartland <mary.gartland@ucdconnect.ie>
2021-12-08 00:26:26 +0000

    Set up cocotiles Junit test. Fixing a bug in cocotile setup

--- CatanJr/src/resources/CocoTiles.java
+++ CatanJr/src/resources/CocoTiles.java
@@ -5,17 +5,17 @@ import main.Interactor;
 
 public class CocoTiles {
 	
-	//------------------------------------------
-	// ---------- Variables --------------------
-	//------------------------------------------
+	//-------------------------------------------------
+	// ---------- Variables ---------------------------
+	//-------------------------------------------------
 	protected ArrayList<CocoTileTypes> CocoTiles = new ArrayList<CocoTileTypes>();	// Array to hold cocotiles
 	
-	// ----- Create an interactor object -------
+	// ----- Create an interactor object --------------
 	Interactor interactor = new Interactor();
 
-	//------------------------------------------
-	// ---------- Constructor ------------------
-	//------------------------------------------
+	//-------------------------------------------------
+	// ---------- Constructor -------------------------
+	//-------------------------------------------------
 	public CocoTiles() { 
 		// Add 20 cocotiles to the Arraylist.
 		for (int i = 0; i <= 2; i++) { 	// There are 3 of each resource cocotile
@@ -23,17 +23,16 @@ public class CocoTiles {
 			this.CocoTiles.add(CocoTileTypes.GoatCutlasses);
 			this.CocoTiles.add(CocoTileTypes.ShipCastle);
 		}
-		for (int i = 0; i <= 11; i++) { // 11 Ghost captain CocoTiles
+		for (int i = 0; i <= 10; i++) { // 11 Ghost captain CocoTiles
 			this.CocoTiles.add(CocoTileTypes.GhostCaptain);
 		}
-		
 		// Shuffle cocotiles
 		Collections.shuffle(CocoTiles);
 	}
 
-	//------------------------------------------
-	// ---------- Method: buyCocoTile ----------
-	//------------------------------------------
+	//-------------------------------------------------
+	// ---------- Method: buyCocoTile -----------------
+	//-------------------------------------------------
 	public CocoTileTypes buyCocoTile() {
 		if (CocoTiles.size() > 0) {
 			CocoTileTypes cocoTileBought = CocoTiles.get(0); 	// Give user 1st element in shuffled ArrayList of cocotiles
@@ -43,5 +42,13 @@ public class CocoTiles {
 			interactor.printMessage("no cocotiles");			// If there are no cocotiles left, the user cannot buy one
 			return null;
 		}
-	}	
+	}
+	
+	//-------------------------------------------------
+	// ---------- Method: get cocotile array ----------
+	// Used for testing purposes only.
+	//-------------------------------------------------
+	public ArrayList<CocoTileTypes> getCocoTiles() {
+		return CocoTiles;
+	}
 }
---
+++ CatanJr/src/testing/TestCocoTiles.java
@@ -0,0 +1,42 @@
+package testing;
+
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import resources.CocoTileTypes;
+import resources.CocoTiles;
+
+class TestCocoTiles {
+	// ---------- Test cocotile construction - ensure random order and correct amount ----------
+	@Test
+	void test() {
+		// Create cocoTile array to test correct setup and shuffling
+		CocoTiles testCocoTiles = new CocoTiles();
+		// Create an unshuffled cocotile array to compare to
+		ArrayList<CocoTileTypes> NotExpectedCocoTiles = new ArrayList<CocoTileTypes>(
+				Arrays.asList(CocoTileTypes.WoodMolasses, CocoTileTypes.WoodMolasses, CocoTileTypes.WoodMolasses,
+						CocoTileTypes.GoatCutlasses, CocoTileTypes.GoatCutlasses, CocoTileTypes.GoatCutlasses,
+						CocoTileTypes.ShipCastle, CocoTileTypes.ShipCastle, CocoTileTypes.ShipCastle,
+						CocoTileTypes.GhostCaptain, CocoTileTypes.GhostCaptain, CocoTileTypes.GhostCaptain,
+						CocoTileTypes.GhostCaptain, CocoTileTypes.GhostCaptain, CocoTileTypes.GhostCaptain,
+						CocoTileTypes.GhostCaptain, CocoTileTypes.GhostCaptain, CocoTileTypes.GhostCaptain,
+						CocoTileTypes.GhostCaptain, CocoTileTypes.GhostCaptain));
+		// Test the sizes are the same, but the ordering is not
+		assertEquals(NotExpectedCocoTiles.size(), testCocoTiles.getCocoTiles().size(), "Cocotiles array size test");
+		assertNotSame(NotExpectedCocoTiles, testCocoTiles.getCocoTiles(), "Cocotiles contest shuffled test");
+	}
+	
+	// ---------- Test cocotile construction - ensure random order and correct amount ----------
+	@Test
+	void testBuyCocotiles() {
+		// Create CocoTile array to test the buyCocoTile method on
+		CocoTiles testCocoTiles = new CocoTiles();
+		testCocoTiles.buyCocoTile();
+		// Create a CocoTile array as the expected array 
+		ArrayList<CocoTileTypes> compareCocoTiles = testCocoTiles.getCocoTiles();
+		compareCocoTiles.remove(0);
+		// Test buyCocoTile() method
+		assertEquals(compareCocoTiles, testCocoTiles.getCocoTiles(), "Cocotiles buy method");
+	}
+}
