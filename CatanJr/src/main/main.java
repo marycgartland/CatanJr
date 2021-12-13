4196906b03fa1b2563736c2fa39e3423df6ab756
emmapender101 <emma.pender@ucdconnect.ie>
2021-12-13 10:57:59 +0000

    Updated sequence diagrams and use case diagrams

--- CatanJr/src/main/main.java
+++ CatanJr/src/main/main.java
@@ -19,7 +19,18 @@ public class main {
 			// TODO: Limit any code repetition
 			// TODO: Error check for players buying cocotiles - entering incorrect options
 			// TODO: Delete options for 1 player and 0 player from "BoardSetup" as these options won't make it that far 
-					// @EP: Do you agree w this?
+					// @EP: Do you agree w this? - EP: Yes
 		// place ship/lair still looks very long and loopy - def could be simplified/tidier if we have time. (not necessary but a would-be-nice)
+		
+		// EP: Notes from meeting:
+		// GameManager should be singleton design pattern: one instance of one class only, maybe use for other classes where we only need one instance
+		// if more than 2/3 classes have rference to the view(interactor class) then there may be a problem, need unified reference to the view through the controller class
+		// implement observer design pattern to checkWinner mehtod, should be seperate method, not in the loop, the subject is the Player beacuse they keep track of the number of lairs,
+		// the observer is the GameManager class, so code should only ne notified if the condition is satisfied (lairs=7)
+		// Class diagrams: show cardinality
+		// Sequence diagrams: include parameters to methods
+		
+		
+		
     }
 }
